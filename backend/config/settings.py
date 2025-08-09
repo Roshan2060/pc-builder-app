f9a34a623ca11bd39e5338e8d0f731afcfd8cd68
Roshan2060 <rbhatt2060@gmail.com>
2025-08-09 20:19:26 +0545

    errors fixing

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 import dj_database_url
 
 # Load environment variables from .env file
-load_dotenv()
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
